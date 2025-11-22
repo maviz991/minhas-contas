e775e5beec3363fa60c82cd133c2b348134b6a11
Matheus Aviz <matheus.aviz27@gmail.com>
2025-11-21 23:08:31 -0300

    Versão estável 21/11/2025 23:08

--- minhas-contas-app/services/api.ts
+++ minhas-contas-app/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // MUITO IMPORTANTE: Use o IP da sua máquina que você encontrou com 'ipconfig'
-const API_URL = 'http://192.168.0.15:3001';
+const API_URL = 'http://192.168.0.9:3001';
 
 const api = axios.create({
   baseURL: API_URL,
